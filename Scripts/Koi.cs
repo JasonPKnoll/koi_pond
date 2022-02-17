dccfea3a1ba707619f510f1733a9a9e1531b91a9
Jason Knoll <jason.p.knoll@gmail.com>
2022-02-17 15:08:00 -0700

    Add raycasting wall detection and avoidance

--- Scripts/Koi.cs
+++ Scripts/Koi.cs
@@ -1,16 +1,18 @@
-
 using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 
+
 public class Koi : UdonSharpBehaviour
 {
-    public float speed = 1.0f;
+    public float speed = 0.0f;
     public float rotationSpeed = 1.0f;
-    public float directionChangeInterval = 1.0f;
+    public float directionChangeInterval = 2.0f;
     private float lastDirectionChangeTime;
     private Quaternion heading;
+    public LayerMask mask;
+    RaycastHit hit;
 
     void Start()
     {
@@ -19,17 +21,61 @@ public class Koi : UdonSharpBehaviour
 
     private void Update()
     {
-        transform.rotation = Quaternion.Lerp(transform.rotation, heading, Time.deltaTime * rotationSpeed);
-        transform.position += transform.forward * speed * Time.deltaTime;
-        if(Time.time > lastDirectionChangeTime + directionChangeInterval)
+        DrawRays();
+    }
+
+    void DrawRays()
+    {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        Vector3 left = transform.TransformDirection(Vector3.left);
+
+        if (Physics.Raycast(transform.position, fwd, 2, mask))
         {
-            ChooseHeading();
+            if (Physics.Raycast(transform.position, right, 2, mask))
+            {
+                //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.right) * 2, Color.red);
+                if (Physics.Raycast(transform.position, left, 2, mask))
+                {
+                    //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.left) * 2, Color.red);
+                }
+                else
+                {
+                    heading = Quaternion.Euler(0.0f, heading.eulerAngles.y - 90.0f, 0.0f);
+                    transform.Rotate(0, -90 * Time.deltaTime, 0);
+
+                    //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.left) * 2, Color.green);
+                    //Debug.Log("" + heading.eulerAngles.y + " Turning Left" + heading);
+                }
+            }
+            else
+            {
+
+                heading = Quaternion.Euler(0.0f, heading.eulerAngles.y + 90.0f, 0.0f);
+                transform.Rotate(0, 90 * Time.deltaTime, 0);
+
+                //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.right) * 2, Color.green);
+                //Debug.Log("" + heading.eulerAngles.y + " Turning Right" + heading);
+            }
+
+            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 2, Color.red);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, heading, Time.deltaTime * rotationSpeed);
+            transform.position += transform.forward * speed * Time.deltaTime;
+
+            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 2, Color.green);
+            if (Time.time > lastDirectionChangeTime + directionChangeInterval)
+            {
+                ChooseHeading();
+            }
         }
     }
 
     void ChooseHeading()
     {
-        heading = Random.rotation;
+        heading = Quaternion.Euler(0.0f, Random.Range(0.0f, 360.0f), 0.0f);
         lastDirectionChangeTime = Time.time;
     }
 }
