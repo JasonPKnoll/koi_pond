6718b578a4ca97b0cb969ff3ebfb58e2ae6e5969
Jason Knoll <jason.p.knoll@gmail.com>
2022-02-23 15:34:29 -0700

    Add food spawner

---
+++ Scripts/FoodSpawner.cs
@@ -0,0 +1,35 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Components;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class FoodSpawner : UdonSharpBehaviour
+{
+    [SerializeField] 
+    public VRCObjectPool availableObjects;
+    public GameObject spawnedObject;
+
+    private Rigidbody _rigidBody;
+
+    void Start()
+    {
+        
+    }
+
+    public override void Interact()
+    {
+        SpawnObject();
+    }
+    public void SpawnObject()
+    {
+        if (!Networking.IsOwner(gameObject)) return;
+        spawnedObject = availableObjects.TryToSpawn();
+        _rigidBody = spawnedObject.GetComponent<Rigidbody>();
+        spawnedObject.transform.position = transform.position + new Vector3(0, 0.5f, 0);
+        _rigidBody.isKinematic = true;
+        _rigidBody.useGravity = false;
+    }
+}
