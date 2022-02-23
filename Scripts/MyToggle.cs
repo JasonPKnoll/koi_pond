769784ca102fcfdf7001c020a937e3a4c5373c5f
Jason Knoll <jason.p.knoll@gmail.com>
2022-02-23 15:40:03 -0700

    Add basic toggle class for unity objects

---
+++ Scripts/MyToggle.cs
@@ -0,0 +1,31 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class MyToggle : UdonSharpBehaviour
+{
+    public GameObject target;
+    public GameObject[] targets;
+    void Start()
+    {
+
+    }
+
+    public override void Interact()
+    {
+        if (targets != null)
+        {
+            foreach (var target in targets)
+            {
+                target.SetActive(!target.activeSelf);
+            }
+        }
+
+        if (target != null)
+        {
+            target.SetActive(!target.activeSelf);
+        }
+    }
+}
