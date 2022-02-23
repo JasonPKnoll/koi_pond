6b8e385d13c0a212457946f50d467e7e331f10b5
Jason Knoll <jason.p.knoll@gmail.com>
2022-02-23 15:44:33 -0700

    Add fish swapper class

---
+++ Scripts/FishSwapper.cs
@@ -0,0 +1,35 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Components;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class FishSwapper : UdonSharpBehaviour
+{ 
+    //[SerializeField]
+    //public VRCObjectPool availableObjects;
+    [SerializeField]
+    FishSpawner _fishSpawner;
+    [SerializeField]
+    GameObject _fish;
+    [SerializeField]
+    Koi _koi;
+
+    void Start()
+    {
+        
+    }
+
+    public void SwapFish(GameObject fish)
+    {
+        _fish = fish;
+        _fishSpawner.availableObjects.Return(_fish);
+        _fish = _fishSpawner.availableObjects.TryToSpawn();
+        //_koi = GetComponent<Koi>();
+        //_koi.SpawnOutOfWater();
+        _koi = _fish.GetComponent<Koi>();
+        _koi.SpawnOutOfWater();
+        _koi.transform.position = transform.position + new Vector3(0, 0.5f, 0);
+    }    
+}
