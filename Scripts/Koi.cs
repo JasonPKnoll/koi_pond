73cf25134ef07f72d75b7e0dc139b0bff6b185a1
Jason Knoll <jason.p.knoll@gmail.com>
2022-03-17 04:23:03 -0700

    Add player sync for fish colors

--- Scripts/Koi.cs
+++ Scripts/Koi.cs
@@ -3,24 +3,51 @@
 using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
+using VRC.SDK3.Components;
 using VRC.Udon;
 
 
 public class Koi : UdonSharpBehaviour
 {
-    public float speed = 0.0f;
     public float rotationSpeed = 1.0f;
     public float directionChangeInterval = 2.0f;
-    private bool fertility = false;
-    public float fishSize = 0.04f;
     private bool swappable = false;
+    public bool outOfWater = false;
+
+    [UdonSynced] public float speed = 0.0f;
+    [UdonSynced] public float fishSize = 0.04f;
+
+    [UdonSynced] public float r = 0f;
+    [UdonSynced] public float g = 0f;
+    [UdonSynced] public float b = 0f;
+
+    [UdonSynced] public float r2 = 0f;
+    [UdonSynced] public float g2 = 0f;
+    [UdonSynced] public float b2 = 0f;
+
+    public float _speed = 0.0f;
+    public float _fishSize = 0.04f;
+
+    public float _r = 0f;
+    public float _g = 0f;
+    public float _b = 0f;
+                 
+    public float _r2 = 0f;
+    public float _g2 = 0f;
+    public float _b2 = 0f;
+
     private float lastDirectionChangeTime;
+
+    private VRCObjectSync sync;
+
+    [UdonSynced]
     private Quaternion heading;
 
     private Renderer _renderer;
-    private MaterialPropertyBlock _propBlock;
     private Rigidbody _rigidBody;
     private Collider _collider;
+    [SerializeField] 
+    MaterialPropertyBlock _propBlock;
 
     // For RayCasting
     private float rayDistance = 0.5f;
@@ -28,16 +55,12 @@ public class Koi : UdonSharpBehaviour
     RaycastHit hit;
 
     // Access to other Classes
-    [SerializeField]
-    CookFish _cookFish;
-    [SerializeField]
-    FishSwapper _fishSwapper;
-    [SerializeField]
-    FoodSpawner _foodSpawner;
-    [SerializeField]
-    FishSpawner _fishSpawner;
-    [SerializeField]
-    GameObject _food;
+    [SerializeField] CookFish _cookFish;
+    [SerializeField] FishSwapper _fishSwapper;
+    [SerializeField] FoodSpawner _foodSpawner;
+    [SerializeField] FishSpawner _fishSpawner;
+    [SerializeField] Food _food;
+
 
     void Start()
     {
@@ -182,10 +205,10 @@ public class Koi : UdonSharpBehaviour
         }
     }
 
-    public void SpawnOutOfWater()
-    {
-        ChooseHeading();
+    public void CreateNewKoi(bool gravity, bool kinematic) {
+        sync = (VRCObjectSync)GetComponent(typeof(VRCObjectSync));
         speed = 0.2f;
+        fishSize = 0.04f;
         swappable = false;
         transform.localScale = new Vector3(fishSize, fishSize, fishSize);
 
@@ -194,13 +217,23 @@ public class Koi : UdonSharpBehaviour
         _rigidBody = GetComponent<Rigidbody>();
         _collider = GetComponent<Collider>();
 
-        _rigidBody.useGravity = true;
-        _rigidBody.isKinematic = false;
-        _propBlock.SetColor("_Color", new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)));
-        _propBlock.SetColor("_Color2", new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)));
+        sync.SetGravity(gravity);
+        sync.SetKinematic(kinematic);
+
+        r = Random.Range(0f, 1f);
+        g = Random.Range(0f, 1f);
+        b = Random.Range(0f, 1f);
+
+        r2 = Random.Range(0f, 1f);
+        g2 = Random.Range(0f, 1f);
+        b2 = Random.Range(0f, 1f);
+
+        _propBlock.SetColor("_Color", new Color(r, g, b));
+        _propBlock.SetColor("_Color2", new Color(r2, g2, b2));
 
         _renderer.SetPropertyBlock(_propBlock);
-    }
+        RequestSerialization();
+    }    
 
     public override void OnPickup()
     {
@@ -215,5 +248,42 @@ public class Koi : UdonSharpBehaviour
     {
         heading = Quaternion.Euler(0.0f, Random.Range(0.0f, 360.0f), 0.0f);
         lastDirectionChangeTime = Time.time;
+    public override void OnDeserialization() {
+        if (r != _r || b != _b || b2 != _b2 || g2 != _g2) {
+            syncNewFish();
+        }
+        if (fishSize != _fishSize) {
+            syncFishGrowwth();
+        }
+    }
+
+    void syncFishGrowwth() {
+        _fishSize = fishSize;
+        _speed = speed;
+        transform.localScale = new Vector3(fishSize, fishSize, fishSize);
+    }
+
+    void syncNewFish() {
+        _r = r;
+        _b = b;
+        _g = g;
+        _r2 = r2;
+        _b2 = b2;
+        _g2 = g2;
+
+        speed = 0.2f;
+        fishSize = 0.04f;
+        swappable = false;
+        transform.localScale = new Vector3(fishSize, fishSize, fishSize);
+
+        _renderer = GetComponent<Renderer>();
+        _propBlock = new MaterialPropertyBlock();
+        _rigidBody = GetComponent<Rigidbody>();
+        _collider = GetComponent<Collider>();
+
+        _propBlock.SetColor("_Color", new Color(r, g, b));
+        _propBlock.SetColor("_Color2", new Color(r2, g2, b2));
+
+        _renderer.SetPropertyBlock(_propBlock);
     }
 }
