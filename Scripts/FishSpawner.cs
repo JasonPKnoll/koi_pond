78af559f56018dfd45424dcf663e000052bb6909
JasonPKnoll <Jason.p.knoll@gmail.com>
2022-04-18 21:29:27 -0700

    Add spawn at location for  fish spawner

--- Scripts/FishSpawner.cs
+++ Scripts/FishSpawner.cs
@@ -2,6 +2,7 @@
 using UdonSharp;
 using UnityEngine;
 using VRC.SDK3.Components;
+using VRC.Udon.Common.Interfaces;
 using VRC.SDKBase;
 using VRC.Udon;
 
@@ -10,6 +11,7 @@ public class FishSpawner : UdonSharpBehaviour
 {
     [SerializeField]
     public VRCObjectPool availableObjects;
+    public Vector3 fishSpawnLocation;
     public GameObject spawnedObject;
     public Koi _koi;
     public float spawnRadius = 3f;
@@ -17,8 +19,7 @@ public class FishSpawner : UdonSharpBehaviour
     private const byte Swimming = 1;
     public VRCObjectSync sync;
 
-    void Start()
-    {
+    void Start() {
         //sync = (VRCObjectSync)GetComponent(typeof(VRCObjectSync));
         for (int i = 0; i < 10; i++) {
             SpawnNearSpawner();
@@ -26,8 +27,7 @@ public class FishSpawner : UdonSharpBehaviour
     }
 
 
-    void SpawnNearSpawner()
-    {
+    void SpawnNearSpawner() {
         spawnedObject = availableObjects.TryToSpawn();
         _koi = spawnedObject.GetComponent<Koi>();
         _koi.transform.position = transform.position + new Vector3(Random.Range(-spawnRadius, spawnRadius), 0f, Random.Range(-spawnRadius, spawnRadius));
@@ -35,11 +35,20 @@ public class FishSpawner : UdonSharpBehaviour
     }
 
 
-    public void RespawnFromFall(GameObject fallenObject)
-    {
+    public void RespawnFromFall(GameObject fallenObject) {
         spawnedObject = fallenObject;
         Rigidbody _rigidbody = spawnedObject.GetComponent<Rigidbody>();
         _rigidbody.velocity = Vector3.zero;
         spawnedObject.transform.position = transform.position + new Vector3(0f, 8f, 0f);
+    }
+
+    public void SpawnInteract() {
+        this.SendCustomNetworkEvent(NetworkEventTarget.Owner, "SpawnAtLocation");
+    }
+
+    public void SpawnAtLocation() {
+        spawnedObject = availableObjects.TryToSpawn();
+        _koi = spawnedObject.GetComponent<Koi>();
+        _koi.transform.position = fishSpawnLocation;
     }
 }
