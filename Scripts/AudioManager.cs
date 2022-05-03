d1dccf5789eeddcef5ab83f2a9ac391011369161
JasonPKnoll <Jason.p.knoll@gmail.com>
2022-05-02 19:45:12 -0700

    Add audio manager class

---
+++ Scripts/AudioManager.cs
@@ -0,0 +1,15 @@
+
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class AudioManager : UdonSharpBehaviour
+{
+
+    public AudioSource[] audioEat;
+    public AudioSource[] audioSplash;
+    public AudioSource[] audioMakingOffspring;
+    public AudioSource[] audioBonk;
+
+}
